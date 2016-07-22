f3ca4cc0cd241410050e27d87daf81533e7df7cf
Ian Merrick <MerrickI@cardiff.ac.uk>
2016-07-22 23:02:05 +0100

    updated the entry point.

--- centos/7/user_entrypoint.sh
+++ centos/7/user_entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash 
 
-# nothing yet...
+#[ -z ${HOST_GROUP_ID+x} ] || \
+#[ -z ${HOST_USER_ID+x} ]  || \
+#(
+if [[ ! -z ${HOST_USER_ID+x} &&  ! -z ${HOST_GROUP_ID+x} ]] ; then
+   groupadd -g $HOST_GROUP_ID -f user 
+   useradd -m -s /bin/bash -u $HOST_USER_ID  -g $HOST_GROUP_ID user
+   su -l user -c $@
+   exit 0
+#)
+else
+ exec $@
+fi
