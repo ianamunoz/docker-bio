0d48c97f97c5afac8ad24c8281554eb2d79ff181
Ian Merrick <MerrickI@cardiff.ac.uk>
2016-07-24 21:55:15 +0100

    Added gosu package to the base image, and utilised in the default entrypoint when a user/group id is specified. Added that user to the sudoers list.

--- centos/7/user_entrypoint.sh
+++ centos/7/user_entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash 
 
-#[ -z ${HOST_GROUP_ID+x} ] || \
-#[ -z ${HOST_USER_ID+x} ]  || \
-#(
 if [[ ! -z ${HOST_USER_ID+x} &&  ! -z ${HOST_GROUP_ID+x} ]] ; then
    groupadd -g $HOST_GROUP_ID -f user 
    useradd -m -s /bin/bash -u $HOST_USER_ID  -g $HOST_GROUP_ID user
-   su -l user -c $@
+   sed -i '/^root/a user    ALL=(ALL)       NOPASSWD: ALL'  /etc/sudoers 
+   /usr/local/sbin/gosu user "$@"
+#   su -l user -c $@
    exit 0
-#)
 else
  exec $@
 fi
