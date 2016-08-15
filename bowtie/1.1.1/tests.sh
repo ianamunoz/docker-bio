11d5504a7ee071914a3fcb6fc3807bd3e4235e2d
Ian Merrick <MerrickI@Cardiff.ac.uk>
2016-08-15 15:32:39 +0100

    Added tests for CI for a number of images

---
+++ bowtie/1.1.1/tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# docker run -i sglim2/bowtie-1.1.1 bash < tests.sh
+#
+
+# test ############
+echo "version test"
+bowtie --version
+###################
+
+# e_coli test
+echo "e_coli test"
+bowtie e_coli /software/applications/bowtie/v1.1.1/reads/e_coli_1000.fq
+###################
---
+++ bwa/0.7.15/tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# docker run -i sglim2/bowtie-1.1.1 bash < tests.sh
+#
+
+# test ############
+echo "No test defined"
+###################
